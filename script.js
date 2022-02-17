38029658f283b276739dd8e34f287708f0972b0d
Victor Avila <victoravila1997@gmail.com>
2022-02-17 16:16:19 -0600

    First iteration with calculator functionality

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector("#equal");
+const display = document.querySelector("#display");
+const clear = document.querySelector("#clear");
+let displayValue = "";
+let num1 = "";
+let operation = "";
+let num2 = "";
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -17,3 +27,49 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
   return operator(num1, num2);
 }
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    displayValue += button.textContent;
+    display.textContent = displayValue;
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (num1 === "") {
+      displayValue = display.textContent;
+      num1 = displayValue;
+    }
+    if (operation != "" && displayValue != "") {
+      num2 = displayValue;
+      displayValue = window[operation](parseFloat(num1), parseFloat(num2));
+      num1 = displayValue;
+      display.textContent = num1;
+      num2 = "";
+      operation = "";
+      displayValue = "";
+    }
+    display.textContent = num1;
+    displayValue = "";
+    operation = button.id;
+    console.log(operation);
+  });
+});
+
+equalButton.addEventListener("click", function () {
+  num2 = displayValue;
+  displayValue = window[operation](parseFloat(num1), parseFloat(num2));
+  num1 = displayValue;
+  display.textContent = num1;
+  num2 = "";
+  operation = "";
+  displayValue = "";
+});
+
+clear.addEventListener("click", function () {
+  num1 = "";
+  num2 = "";
+  displayValue = "";
+  display.textContent = 0;
+});
